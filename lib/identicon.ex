40fd4e82097bfaf037520f84f89c24fdd4e97648
Syed Aman <sna274@nyu.edu>
2018-03-10 20:16:24 -0600

    provide better method name

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,11 +1,11 @@
 defmodule Identicon do
-  def string_to_hash(string_to_encode) do
+  def string_to_hash_list(string_to_encode) do
     :crypto.hash(:md5, string_to_encode)
     |> :binary.bin_to_list
   end
 
   def string_to_identicon(string_to_convert) do
     string_to_convert
-    |> string_to_hash
+    |> string_to_hash_list
   end
 end
