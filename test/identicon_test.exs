31e6b1f685b0cdea7766197576f9358f5e2f00d7
Syed Aman <sna274@nyu.edu>
2018-03-10 20:00:42 -0600

    create some tests

--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -2,7 +2,14 @@ defmodule IdenticonTest do
   use ExUnit.Case
   doctest Identicon
 
-  test "greets the world" do
-    assert Identicon.hello() == :world
+  # test "should convert a string to an identicon" do
+    # Identicon.string_to_identicon('syed')
+  # end
+
+  test "should consistently generate the same identicon for the same string" do
+    first_identicon = Identicon.string_to_identicon('syed')
+    second_identicon = Identicon.string_to_identicon('syed')
+
+    assert first_identicon == second_identicon
   end
 end
