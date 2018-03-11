4d0bbb6936f0aee7bbe86f8a2392aa4c756a44c7
Syed Aman <sna274@nyu.edu>
2018-03-10 20:12:01 -0600

    add some more unit tests via TDD

--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -2,14 +2,27 @@ defmodule IdenticonTest do
   use ExUnit.Case
   doctest Identicon
 
-  # test "should convert a string to an identicon" do
+  test "should generate an identicon from a string" do
     # Identicon.string_to_identicon('syed')
-  # end
+  end
 
   test "should consistently generate the same identicon for the same string" do
     first_identicon = Identicon.string_to_identicon('syed')
     second_identicon = Identicon.string_to_identicon('syed')
 
     assert first_identicon == second_identicon
+  end
+
+  test "should generate an identicon that mirrors columns 1 and 2 with columns
+  4 and 5" do
+  end
+
+  test "should generate an identicon that is 5 boxes long and 5 boxeswide" do
+  end
+
+  test "should have colored boxes that correspond to even hash values" do
+  end
+
+  test "should have white boxes that correspond to odd hash values" do
   end
 end
