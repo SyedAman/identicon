458aa92bd39c0f9ee8143b0c43c52b9c8e7fa059
Syed Aman <sna274@nyu.edu>
2018-03-10 20:50:12 -0600

    get rgb values from first three elements in hex list

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,11 +1,22 @@
 defmodule Identicon do
-  def string_to_hash_list(string_to_encode) do
+  def string_to_hex_list(string_to_encode) do
     :crypto.hash(:md5, string_to_encode)
     |> :binary.bin_to_list
   end
 
+  def form_image_from_hex_list(hex_list) do
+    %Identicon.Image{hex: hex_list}
+  end
+
+  def generate_box_color(image) do
+    %Identicon.Image{hex: [r, g, b | _tail]} = image
+    [r, g, b]
+  end
+
   def string_to_identicon(string_to_convert) do
     string_to_convert
-    |> string_to_hash_list
+    |> string_to_hex_list
+    |> form_image_from_hex_list
+    |> generate_box_color
   end
 end
