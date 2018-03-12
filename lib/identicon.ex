4991e3f073446e8ed24ef021bdadd051e745a5e0
Syed Aman <sna274@nyu.edu>
2018-03-11 21:09:05 -0500

    format

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,7 +1,7 @@
 defmodule Identicon do
   def create_hex_list(string_to_encode) do
     :crypto.hash(:md5, string_to_encode)
-    |> :binary.bin_to_list
+    |> :binary.bin_to_list()
   end
 
   def build_image(hex_list) do
@@ -13,15 +13,16 @@ defmodule Identicon do
   end
 
   def mirror_row([column1, column2 | _tail] = row) do
-    row ++ [column2,  column1]
+    row ++ [column2, column1]
   end
 
   def build_grid(%Identicon.Image{hex: hex_list} = image) do
-    grid_rows = hex_list
-    |> Enum.chunk(3)
-    |> Enum.map(&mirror_row/1)
-    |> List.flatten
-    |> Enum.with_index 
+    grid_rows =
+      hex_list
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
 
     %Identicon.Image{image | grid: grid_rows}
   end
